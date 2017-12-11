93a917893e18920b75e7c663a4e5769950c84e9a
Ilkyu Ju <me@ilkyu.kr>
2017-12-11 09:47:36 +0900

    Fix #1
    yay

--- romanizer.py
+++ romanizer.py
@@ -93,6 +93,20 @@ coda = {
     None: '',
 }
 
+double_consonant_final = {
+    'ㄳ' : ('ㄱ', 'ㅅ'),
+    'ㄵ' : ('ᆫ', 'ㅈ'), 
+    'ᆭ' : ('ᆫ', 'ᇂ'),
+    'ㄺ' : ('ㄹ', 'ㄱ'), 
+    'ㄻ' : ('ㄹ', 'ㅁ'), 
+    'ㄼ' : ('ㄹ', 'ㅂ'), 
+    'ㄽ' : ('ㄹ', 'ㅅ'), 
+    'ㄾ' : ('ㄹ', 'ㅌ'), 
+    'ㄿ' : ('ㄹ', 'ㅍ'),
+    'ㅀ' : ('ㄹ', 'ᇂ'), 
+    'ㅄ' : ('ㅂ', 'ㅅ'), 
+    'ㅆ' : ('ㅅ', 'ㅅ')
+}
 
 NULL_CONSONANT = 'ᄋ'
 
@@ -184,6 +198,11 @@ class Pronouncer(object):
                 final_is_before_C = syllable.final and next_syllable.getattr(initial) not in (None, NULL_CONSONANT)
             except AttributeError:
                 final_is_before_C = False
+                
+            try:    
+                final_is_before_V = syllable.final and next_syllable.initial is None
+            except AttributeError:
+                final_is_before_V = False 
             
             # 1. 받침 ‘ㄲ, ㅋ’, ‘ㅅ, ㅆ, ㅈ, ㅊ, ㅌ’, ‘ㅍ’은 어말 또는 자음 앞에서 각각 대표음 [ㄱ, ㄷ, ㅂ]으로 발음한다.
             # 2. 겹받침 ‘ㄳ’, ‘ㄵ’, ‘ㄼ, ㄽ, ㄾ’, ‘ㅄ’은 어말 또는 자음 앞에서 각각 [ㄱ, ㄴ, ㄹ, ㅂ]으로 발음한다.
@@ -205,7 +224,8 @@ class Pronouncer(object):
             
             
             # 4. 받침 ‘ㅎ’의 발음은 다음과 같다.
-            if(syllable.final == 'ᇂ'):
+            if syllable.final in ['ᇂ', 'ᆭ', 'ᆶ']:
+                
                 if next_syllable:
                     # ‘ㅎ(ㄶ, ㅀ)’ 뒤에 ‘ㄱ, ㄷ, ㅈ’이 결합되는 경우에는, 뒤 음절 첫소리와 합쳐서 [ㅋ, ㅌ, ㅊ]으로 발음한다.
                     # ‘ㅎ(ㄶ, ㅀ)’ 뒤에 ‘ㅅ’이 결합되는 경우에는, ‘ㅅ’을 [ㅆ]으로 발음한다.
@@ -216,12 +236,29 @@ class Pronouncer(object):
                     # 3. ‘ㅎ’ 뒤에 ‘ㄴ’이 결합되는 경우에는, [ㄴ]으로 발음한다.
                     elif next_syllable.initial in ['ᄂ']:
                         # TODO: [붙임] ‘ㄶ, ㅀ’ 뒤에 ‘ㄴ’이 결합되는 경우에는, ‘ㅎ’을 발음하지 않는다.
-                        syllable.final = 'ᆫ'
+                        if(syllable.final in ['ᆭ', 'ᆶ']):
+                            if syllable.final == 'ᆭ':
+                                syllable.final = 'ᆫ'
+                            elif syllable.final == 'ᆶ':
+                                syllable.final = 'ᆯ' 
+                        else:
+                            syllable.final = 'ᆫ'
+                    #4. ‘ㅎ(ㄶ, ㅀ)’ 뒤에 모음으로 시작된 어미나 접미사가 결합되는 경우에는, 
+                    # ‘ㅎ’을 발음하지 않는다.
                     elif next_syllable.initial == NULL_CONSONANT:
-                        syllable.final = None
-                    else: syllable.final =None
+                        if(syllable.final in ['ᆭ', 'ᆶ']):
+                            if syllable.final == 'ᆭ':
+                                syllable.final = 'ᆫ'
+                            elif syllable.final == 'ᆶ':
+                                syllable.final = 'ᆯ' 
+                        else:
+                            syllable.final = None
+                    else:
+                        if (syllable.final == 'ᇂ'):
+                            syllable.final = None
                 else:
-                    syllable.final = None
+                    if (syllable.final == 'ᇂ'):
+                        syllable.final = None
             # 5. 홑받침이나 쌍받침이 모음으로 시작된 조사나 어미, 접미사와 결합되는 경우에는, 
             # 제 음가대로 뒤 음절 첫소리로 옮겨 발음한다. 
             if next_syllable and final_is_before_C:
@@ -229,9 +266,17 @@ class Pronouncer(object):
                     next_syllable.initial = next_syllable.final_to_initial(syllable.final)
                     syllable.final = None
                     
+            # 6. 겹받침이 모음으로 시작된 조사나 어미, 접미사와 결합되는 경우에는, 
+            # 뒤엣것만을 뒤 음절 첫소리로 옮겨 발음한다.(이 경우, ‘ㅅ’은 된소리로 발음함.)
+            #print(syllable.final in double_consonant_final)
+            # print(syllable.final)
+            if syllable.final in double_consonant_final:
+                double_consonant = double_consonant_final[syllable.final]
+                syllable.final = double_consonant[0]
+                next_syllable.initial = next_syllable.final_to_initial(double_consonant[1])
         return self._syllables
-                
 
+    
 class Romanizer(object):
     def __init__(self, text):
         self.text = text
