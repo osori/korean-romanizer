3e6f31b86cd906dc86b19fed34612e2c7b173be7
Ilkyu Ju <ju.ilkyu@gmail.com>
2021-08-18 21:59:45 +0900

    trying to deploy romanizer serverless functions -- 1st try

---
+++ api/romanize_test.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+from korean_romanizer.romanizer import Romanizer
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+
+    r = Romanizer("안녕하세요")
+    romanized = r.romanize()
+    self.wfile.write(romanized)
+    return
