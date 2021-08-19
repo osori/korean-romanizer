e0b81adcc12b538bb6df0f8431b56caf5d50d494
Ilkyu Ju <ju.ilkyu@gmail.com>
2021-08-19 16:58:31 +0900

    set content type charset to utf-8

--- api/romanize.py
+++ api/romanize.py
@@ -6,7 +6,7 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','text/plain; charset=utf-8')
         self.end_headers()
 
 
