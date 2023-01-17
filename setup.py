f89aa57cbe4824682c8db90564fbf3dad5bcd52d
Ilkyu Ju <ju.ilkyu@gmail.com>
2023-01-18 01:11:00 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'korean_romanizer',
   packages = ['korean_romanizer'],
-  version = '0.24',
+  version = '0.25',
   license='GNU GPLv3',
   description = 'A Python library for Korean romanization',
   author = 'Ilkyu Ju',
   author_email = 'ju.ilkyu@gmail.com',
   url = 'https://github.com/osori/korean-romanizer',
-  download_url = 'https://github.com/osori/korean-romanizer/archive/0.24.tar.gz',
+  download_url = 'https://github.com/osori/korean-romanizer/archive/0.25.tar.gz',
   keywords = ['Korean', 'Romanization', 'Transliteration'],
   classifiers=[
     'Development Status :: 3 - Alpha',
