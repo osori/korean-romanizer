f64af20aafa5a42c37f48ed28f0f3f4ab457011e
duong <duongnguyenthai78@gmail.com>
2025-04-01 23:26:20 -0400

    Clean up a bit

--- korean_romanizer/pronouncer.py
+++ korean_romanizer/pronouncer.py
@@ -105,12 +105,15 @@ class Pronouncer(object):
                         syllable.final = None
             # 6. 겹받침이 모음으로 시작된 조사나 어미, 접미사와 결합되는 경우에는,
             # 뒤엣것만을 뒤 음절 첫소리로 옮겨 발음한다.(이 경우, ‘ㅅ’은 된소리로 발음함.)
-            if syllable.final in double_consonant_final and next_syllable and next_syllable.initial == NULL_CONSONANT:
-
-                double_consonant = double_consonant_final[syllable.final]
-                syllable.final = double_consonant[0]
-                next_syllable.initial = next_syllable.final_to_initial(
-                    double_consonant[1])
+            if syllable.final in double_consonant_final:
+                if not next_syllable:
+                    syllable.final = double_consonant_final[syllable.final][0]
+                elif next_syllable.initial == NULL_CONSONANT:
+                    double_consonant = double_consonant_final[syllable.final]
+                    syllable.final = double_consonant[0]
+                    next_syllable.initial = next_syllable.final_to_initial(
+                        double_consonant[1])
+                    
             # 5. 홑받침이나 쌍받침이 모음으로 시작된 조사나 어미, 접미사와 결합되는 경우에는,
             # 제 음가대로 뒤 음절 첫소리로 옮겨 발음한다.
             if next_syllable and final_is_before_V:
@@ -119,8 +122,5 @@ class Pronouncer(object):
                     next_syllable.initial = next_syllable.final_to_initial(
                         syllable.final)
                     syllable.final = None
-                    
-            if not next_syllable and syllable.final in double_consonant_final:
-                syllable.final = double_consonant_final[syllable.final][0]
 
         return self._syllables
